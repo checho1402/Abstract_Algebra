4ca396154fd12551cdb3e472f4131d00364d21e3
checho1402 <80992731+checho1402@users.noreply.github.com>
2021-04-16 12:18:06 -0500

    Commit int a string

---
+++ Practica manipulacion de string/int a string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  long int cadena1 = 548365842358;
+
+  string string_int (to_string(cadena1));
+
+  cout << "\nEl número es (string): "<<cadena1<<"\n";
+}
+
+

7805d04ac3a1277ceed7fa0cb0c41ed37b2e790c
checho1402 <80992731+checho1402@users.noreply.github.com>
2021-04-16 12:17:42 -0500

    commit string a int

---
+++ Practica manipulacion de string/String a int.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string cadena1 = "548365842358";
+
+  int string_int = atoi(cadena1.c_str());
+
+  cout << "\nEl numero es (int): "<<cadena1<<"\n";
+}
+
